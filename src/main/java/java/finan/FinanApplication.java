9641dad4a23a36c90e8984e77dd09903b9f7b8a2
pa.k.petrov <pa.k.petrov@tinkoff.ru>
2022-09-01 22:39:24 +0300

    add spring dependencies

---
+++ src/main/java/java/finan/FinanApplication.java
@@ -0,0 +1,13 @@
+package java.finan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java/finan/FinanApplicationTests.java
@@ -0,0 +1,13 @@
+package java.finan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
